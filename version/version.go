0d65bbb0e1ff257c79526fbeccf618c8295ffed8
David Vennik <david@cybriq.systems>
2022-07-09 10:11:22 +0300

    syncing up versions
    note to self: create script to do this properly automatically

--- version/version.go
+++ version/version.go
@@ -12,12 +12,12 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname
 	GitRef = "refs/heads/master"
 	// GitCommit is the commit hash of the current HEAD
-	GitCommit = "7bbe20b4197e58e42919d9d09a7feec1cbccbf57"
+	GitCommit = "04c0337475e9302805d4b98ccbe02dfdadf4d6a4"
 	// BuildTime stores the time when the current binary was built
-	BuildTime = "2022-07-09T07:53:53+03:00"
+	BuildTime = "2022-07-09T10:10:48+03:00"
 	// Tag lists the Tag on the build, adding a + to the newest Tag if the commit is
 	// not that commit
-	Tag = "v0.0.6"
+	Tag = "v0.0.9"
 	// PathBase is the path base returned from runtime caller
 	PathBase = "/home/davidvennik/src/gitlab.com/cybriqsystems/proc/"
 	// Major is the major number from the tag
@@ -25,7 +25,7 @@ var (
 	// Minor is the minor number from the tag
 	Minor = 0
 	// Patch is the patch version number from the tag
-	Patch = 6
+	Patch = 9
 )
 
 // Get returns a pretty printed version information string
