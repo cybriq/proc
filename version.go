23bb0bcdd2c16a4f3a6db435e4a17280c839ae31
David Vennik <david@cybriq.systems>
2022-07-09 17:40:44 +0300

    now allows creating github conformant commits
    with newlines easily added using double hyphen
    
    like this

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname
 	GitRef = "refs/heads/master"
 	// GitCommit is the commit hash of the current HEAD
-	GitCommit = "212332f07e23fd05c6064ea196646b8239808c7d"
+	GitCommit = "34e5d18e0724a84caf5082a2fb3fe5db1d878836"
 	// BuildTime stores the time when the current binary was built
-	BuildTime = "2022-07-09T17:35:55+03:00"
+	BuildTime = "2022-07-09T17:40:44+03:00"
 	// SemVer lists the (latest) git tag on the build
-	SemVer = "v0.0.18"
+	SemVer = "v0.0.19"
 	// PathBase is the path base returned from runtime caller
 	PathBase = "/home/davidvennik/src/gitlab.com/cybriqsystems/proc/"
 	// Major is the major number from the tag
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag
 	Minor = 0
 	// Patch is the patch version number from the tag
-	Patch = 18
+	Patch = 19
 )
 
 // Version returns a pretty printed version information string
